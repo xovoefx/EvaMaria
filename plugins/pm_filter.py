e438cf3d184601b4ff109b42d93e1edd1d859330
xovoefx <98139422+xovoefx@users.noreply.github.com>
2022-11-21 19:22:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -439,7 +439,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         buttons = [[
             InlineKeyboardButton('🤖 Updates', url='https://t.me/venombothelp'),
-            InlineKeyboardButton('♥️ Source', url='https://t.me/biriyanikadaofficial')
+            InlineKeyboardButton('♥️ Source', callback_data='source')
         ], [
             InlineKeyboardButton('🏠 Home', callback_data='start'),
             InlineKeyboardButton('🔐 Close', callback_data='close_data')
